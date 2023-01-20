50b47f5f1a8a0360b250c931adfd6e7906e6a801
tonkhao <chananchida_tha@cmu.ac.th>
2023-01-20 17:27:11 +0700

    kkkkkkkkk

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 using namespace std;
 
+void printO(int N , int M){
+	if(N > 0){
+		for(int i = 0 ; i < N ; i++){
+			if(M > 0){
+				for(int x = 0 ; x < M ; x++){
+					cout <<  "O";
+				}
+				cout << "\n";
+			}
+			else{
+				cout << "Invalid input";
+				break;
+			}
+			}
+		}
+	else{
+		cout << "Invalid input";
+	}
+}
+
 //Write the function printO() here
 
 int main(){
